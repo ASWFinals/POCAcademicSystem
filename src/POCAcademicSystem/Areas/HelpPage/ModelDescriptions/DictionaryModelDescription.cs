e603b6a9b1504550fe99cbff20008831a8484c3d
renanroncarati <renanroncarati@gmail.com>
2016-05-27 22:11:12 -0300

    create an api sample for pocacademic. closes #12

---
+++ src/POCAcademicSystem/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace POCAcademicSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/POCAcademicSystem/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace POCAcademicSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
