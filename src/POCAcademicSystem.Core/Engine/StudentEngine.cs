db2ac2af8bacc2e13260e72c608e4d75cf449ffb
renanroncarati <renanroncarati@gmail.com>
2016-05-29 00:48:47 -0300

    create scafolding for domain class library. closes #15

---
+++ src/POCAcademicSystem.Core/Engine/StudentEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCAcademicSystem.Domain.Engine;
+using POCAcademicSystem.Persistence;
+using POCAcademicSystem.Persistence.Repository;
+using Takenet.Library.Data;
+
+namespace POCAcademicSystem.Core.Engine
+{
+    public class StudentEngine : IStudentEngine
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentEngine(IPOCAcademicContext context)
+        {
+            _unitOfWork = context;
+            _studentRepository = context.StudentRepository;
+        }
+
+        //TODO: regras de nogocio
+    }
+}
--- src/POCAcademicSystem.Persistence/IPOCAcademicContext.cs
+++ src/POCAcademicSystem.Persistence/IPOCAcademicContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using POCAcademicSystem.Persistence.Repository;
 using Takenet.Library.Data;
 
 namespace POCAcademicSystem.Persistence
 {
     public interface IPOCAcademicContext : IUnitOfWork
     {
+        IStudentRepository StudentRepository {get;};
     }
 }
