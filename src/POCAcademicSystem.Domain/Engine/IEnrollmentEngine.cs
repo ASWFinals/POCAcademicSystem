de84f5d42ca0d4378143af074c46bff9d84c24f3
renanroncarati <renanroncarati@gmail.com>
2016-06-17 11:58:53 -0300

    create enrollment engine interface and controller

---
+++ src/POCAcademicSystem.Domain/Engine/IEnrollmentEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCAcademicSystem.Domain.Model;
+
+namespace POCAcademicSystem.Domain.Engine
+{
+    public interface IEnrollmentEngine
+    {
+        int Create(EnrollmentDomain enrollmentDomain);
+
+        EnrollmentDomain Get(int id);
+
+        void Update(EnrollmentDomain enrollmentDomain);
+
+        void Delete(int id);
+
+        IEnumerable<EnrollmentDomain> GetAll();
+    }
+}
---
+++ src/POCAcademicSystem/Controllers/EnrollmentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace POCAcademicSystem.Controllers
+{
+    public class EnrollmentController : ApiController
+    {
+        // GET: api/Enrollment
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Enrollment/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Enrollment
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Enrollment/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Enrollment/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
