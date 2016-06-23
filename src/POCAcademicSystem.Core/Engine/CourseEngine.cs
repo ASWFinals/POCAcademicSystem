26c7fe1dad7007ce476ed9003873ea1a5c431f16
renanroncarati <renanroncarati@gmail.com>
2016-06-23 16:50:26 -0300

    create crud operations on CourseEngine. closes #25

---
+++ src/POCAcademicSystem.Core/Engine/CourseEngine.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCAcademicSystem.Domain.Engine;
+using POCAcademicSystem.Domain.Exceptions;
+using POCAcademicSystem.Persistence;
+using POCAcademicSystem.Persistence.Repository;
+using Takenet.Library.Data;
+using POCAcademicSystem.Core.Translators;
+using Omu.ValueInjecter;
+
+namespace POCAcademicSystem.Core.Engine
+{
+    public class CourseEngine : ICourseEngine
+    {
+        private readonly ICourseRepository _courseRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public CourseEngine(IPOCAcademicContext context)
+        {
+            _unitOfWork = context;
+            _courseRepository = context.CourseRepository;
+        }
+
+        public int Create(Domain.Model.CourseDomain course)
+        {
+            if (course == null)
+            {
+                throw new InvalidRequestException("Entity course is null.");
+            }
+
+            var courseModel = course.ToPersistence();
+
+            _courseRepository.Add(courseModel, true);
+            _unitOfWork.Save();
+
+            return courseModel.CourseId;
+        }
+
+        public void Update(Domain.Model.CourseDomain course)
+        {
+            if (course == null)
+            {
+                throw new InvalidRequestException("Entity course is null.");
+            }
+
+            if (course.CourseId != 0)
+            {
+                var courseModel = _courseRepository.GetById(course.CourseId);
+                courseModel.InjectFrom(course);
+
+                _courseRepository.Add(courseModel, false);
+                _unitOfWork.Save();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var courseModel = _courseRepository.GetById(id);
+
+            if (courseModel == null)
+            {
+                throw new NotFoundException("Entity doesn't exists");
+            }
+
+            //intention
+            _courseRepository.Remove(courseModel);
+            //commit
+            _unitOfWork.Save();
+        }
+
+        public Domain.Model.CourseDomain Get(int id)
+        {
+            var courseModel = _courseRepository.GetById(id);
+
+            if (courseModel == null)
+            {
+                throw new NotFoundException("Entity doesn't exists");
+            }
+
+            return courseModel.ToDomain();
+        }
+
+        public IEnumerable<Domain.Model.CourseDomain> GetAll()
+        {
+            var courseModels = _courseRepository.AsQueryable();
+
+            if (courseModels.Any())
+            {
+                //translate each entity repository
+                return courseModels.ToList().Select(c => c.ToDomain());
+            }
+
+            throw new NotFoundException("There are no course");
+        }
+    }
+}
---
+++ src/POCAcademicSystem.Domain/Engine/ICourseEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCAcademicSystem.Domain.Model;
+
+namespace POCAcademicSystem.Domain.Engine
+{
+    public interface ICourseEngine
+    {
+        int Create(CourseDomain course);
+
+        void Update(CourseDomain course);
+
+        void Delete(int id);
+
+        CourseDomain Get(int id);
+
+        IEnumerable<CourseDomain> GetAll();
+    }
+}
