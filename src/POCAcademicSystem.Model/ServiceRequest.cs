55058d90982327741282017270cfba93ab69adca
renanroncarati <renanroncarati@gmail.com>
2016-06-27 22:09:21 -0300

    create servicerequest model. closes #29

---
+++ src/POCAcademicSystem.Model/ServiceRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POCAcademicSystem.Model
+{
+    public class ServiceRequest
+    {
+        public int ServiceRequestId { get; set; }
+        public short ServiceTypeId { get; set; }
+        public DateTime? RequestDateTime { get; set; }
+        public DateTime? ResponseDateTime { get; set; }
+
+        public virtual ServiceType ServiceType { get; set; }
+    }
+}
