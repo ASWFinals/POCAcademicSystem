873f329e81de36e43fa257bff4b76d1587789f86
renanroncarati <renanroncarati@gmail.com>
2016-06-27 22:36:46 -0300

    create servicetype mapping for table creation. fluent api

---
+++ src/POCAcademicSystem.EntityFramework/Mappings/ServiceTypeMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCAcademicSystem.Model;
+
+namespace POCAcademicSystem.EntityFramework.Mappings
+{
+    public class ServiceTypeMapping : EntityTypeConfiguration<ServiceType>
+    {
+        public ServiceTypeMapping()
+        {
+            ToTable("ServiceType", "dbo");
+            HasKey(s => s.ServiceTypeId);
+
+            Property(s => s.Description)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+        }
+    }
+}
